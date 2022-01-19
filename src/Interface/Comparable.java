12ab80a13fe5c306e8f29c2b80c92ab96bd669ab
DNY <dannylu1999@gmail.com>
2022-01-19 12:48:50 +0100

    StringMethods.java + Loops.java

--- src/Interface/Comparable.java
+++ src/Interface/Comparable.java
@@ -2,4 +2,5 @@ package Interface;//Alle er public abstract i et interface
 
 public interface Comparable { //Her laves en lokal comparable
     int compareTo(Object o);
+    // String color = "Blue";
 }
